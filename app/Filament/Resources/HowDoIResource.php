a7176e03444f16300c9d65c81c0e0578d9a288c4
bashirii <bashiriidd3@gmail.com>
2024-12-03 09:48:29 +0300

    Add markdowon input field

--- app/Filament/Resources/HowDoIResource.php
+++ app/Filament/Resources/HowDoIResource.php
@@ -2,18 +2,14 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\HowDoIResource\Pages;
-use App\Filament\Resources\HowDoIResource\RelationManagers;
-use App\Models\HowDoI;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
 use Filament\Tables;
-use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Models\HowDoI;
 use Filament\Forms\Set;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
 use Illuminate\Support\Str;
+use Filament\Resources\Resource;
 use Filament\Forms\Components\Grid;
 use Filament\Forms\Components\Tabs;
 use Illuminate\Support\Facades\Auth;
@@ -27,8 +23,13 @@ use Illuminate\Database\Eloquent\Model;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\FileUpload;
+use Illuminate\Database\Eloquent\Builder;
 use Filament\Forms\Components\Placeholder;
 use Illuminate\Contracts\Support\Htmlable;
+use Filament\Forms\Components\MarkdownEditor;
+use App\Filament\Resources\HowDoIResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\HowDoIResource\RelationManagers;
 
 class HowDoIResource extends Resource
 {
@@ -36,22 +37,22 @@ class HowDoIResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
-     // name to be used in navigation
-     protected static ?string $navigationLabel = 'How Do I';
+    // name to be used in navigation
+    protected static ?string $navigationLabel = 'How Do I';
 
-     // position of the resource in navigation
-     protected static ?int $navigationSort = 4;
+    // position of the resource in navigation
+    protected static ?int $navigationSort = 4;
 
-     // name to be used in page titles
-     protected static ?string $modelLabel = 'HowDoI';
+    // name to be used in page titles
+    protected static ?string $modelLabel = 'How Do I';
 
-     // navigation group to be used in navigation
-     protected static ?string $navigationGroup = 'Information Hub';
+    // navigation group to be used in navigation
+    protected static ?string $navigationGroup = 'Information Hub';
 
-     // slug to be used in route names abd urls
-     protected static ?string $slug = 'how_do_i';
+    // slug to be used in route names abd urls
+    protected static ?string $slug = 'how_do_i';
 
-      // multiple fields global search with annotation
+    // multiple fields global search with annotation
     public static function getGloballySearchableAttributes(): array
     {
         return ['en_title', 'sw_title'];
@@ -71,72 +72,76 @@ class HowDoIResource extends Resource
         return $form
             ->schema([
                 Section::make()
-                ->description("Fill all required fields on both tabs")
-                ->schema([
-                    Tabs::make('Tabs')
-                        ->tabs([
-                            Tabs\Tab::make('Swahili')
-                                ->schema([
-                                    TextInput::make('sw_title')
-                                        ->label('Title')
-                                        ->required(),
+                    ->description("Fill all required fields on both tabs")
+                    ->schema([
+                        Tabs::make('Tabs')
+                            ->tabs([
+                                Tabs\Tab::make('Swahili')
+                                    ->schema([
+                                        TextInput::make('sw_title')
+                                            ->label('Kichwa cha habari')
+                                            ->required(),
 
-                                    Textarea::make('sw_description')
-                                        ->label('Description')
-                                        ->required(),
+                                        MarkdownEditor::make('sw_description')
+                                            ->disableToolbarButtons([
+                                                'attachFiles',
+                                            ])
+                                            ->label('Maelezo')
+                                            ->required(),
 
-                                ]),
+                                    ]),
 
-                            Tabs\Tab::make('English')
-                                ->schema([
-                                    TextInput::make('en_title')
-                                        ->label('Title')
-                                        ->required()
-                                        ->live(onBlur: true)
-                                        ->afterStateUpdated(fn(string $operation, $state, Set $set) => $operation
-                                            === 'create' ? $set('slug', Str::slug($state)) : null),
+                                Tabs\Tab::make('English')
+                                    ->schema([
+                                        TextInput::make('en_title')
+                                            ->label('Title')
+                                            ->required()
+                                            ->live(onBlur: true)
+                                            ->afterStateUpdated(fn(string $operation, $state, Set $set) => $operation
+                                                === 'create' ? $set('slug', Str::slug($state)) : null),
 
-                                    TextInput::make('slug')
-                                        ->required()
-                                        ->disabled()
-                                        ->dehydrated()
-                                        // ->hidden()
-                                        ->unique(HowDoI::class, 'slug', ignoreRecord: true),
+                                        TextInput::make('slug')
+                                            ->required()
+                                            ->disabled()
+                                            ->dehydrated()
+                                            // ->hidden()
+                                            ->unique(HowDoI::class, 'slug', ignoreRecord: true),
 
-                                    Textarea::make('en_description')
-                                        ->label('Description')
-                                        ->required(),
+                                        MarkdownEditor::make('en_description')
+                                            ->disableToolbarButtons([
+                                                'attachFiles',
+                                            ])
+                                            ->label('Description')
+                                            ->required(),
 
-                                ])
+                                    ])
 
-                        ])->activeTab(1)->columnSpanFull()
+                            ])->activeTab(1)->columnSpanFull()
 
-                ]),
+                    ]),
 
                 Section::make()
-                ->schema([
-                    FileUpload::make('image')
-                        ->image()
-                        ->directory('how_do_')
-                        ->columnSpanFull(),
-                        ]),
-                    DatePicker::make('created_at')
-                        ->nullable()
-                        ->columnSpanFull(),
+                    ->schema([
+                        FileUpload::make('image')
+                            ->image()
+                            ->directory('how_do_i_s')
+                            ->columnSpanFull(),
+                    ]),
+                DatePicker::make('created_at')
+                    ->nullable()
+                    ->columnSpanFull(),
 
-                    Hidden::make('created_by')
-                        ->default(fn() => Auth::id()),
+                Hidden::make('created_by')
+                    ->default(fn() => Auth::id()),
 
-                    Placeholder::make('created_by_name')
-                        ->label('Created By')
-                        ->content(fn() => Auth::user()->name),
+                Placeholder::make('created_by_name')
+                    ->label('Created By')
+                    ->content(fn() => Auth::user()->name),
 
-                    Toggle::make('is_active')
-                        ->required()
-                        ->default(true)
-                ])->columns(2);
-
-
+                Toggle::make('is_active')
+                    ->required()
+                    ->default(true)
+            ])->columns(2);
     }
 
     public static function table(Table $table): Table
@@ -144,44 +149,44 @@ class HowDoIResource extends Resource
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make('sw_title')
-                ->label("Swahili title")
-                ->searchable()
-                ->formatStateUsing(function ($state) {
-                    return Str::words($state, 5, '.....');
-                }),
+                    ->label("Swahili title")
+                    ->searchable()
+                    ->formatStateUsing(function ($state) {
+                        return Str::words($state, 5, '.....');
+                    }),
 
-            Tables\Columns\TextColumn::make('en_title')
-                ->label("English title")
-                ->searchable()
-                ->formatStateUsing(function ($state) {
-                    return Str::words($state, 5, '.....');
-                }),
+                Tables\Columns\TextColumn::make('en_title')
+                    ->label("English title")
+                    ->searchable()
+                    ->formatStateUsing(function ($state) {
+                        return Str::words($state, 5, '.....');
+                    }),
 
-            Tables\Columns\TextColumn::make('user.name')
-                ->label('Created By')
-                ->searchable(query: function (Builder $query, string $search): Builder {
-                    return $query->whereHas('user', function (Builder $query) use ($search) {
-                        $query->where('name', 'like', "%{$search}%");
-                    });
-                }),
+                Tables\Columns\TextColumn::make('user.name')
+                    ->label('Created By')
+                    ->searchable(query: function (Builder $query, string $search): Builder {
+                        return $query->whereHas('user', function (Builder $query) use ($search) {
+                            $query->where('name', 'like', "%{$search}%");
+                        });
+                    }),
 
-            IconColumn::make('is_active')
-                ->label('Status')
-                ->boolean()
-                ->trueIcon('heroicon-o-check-circle')
-                ->falseIcon('heroicon-o-x-circle')
-                ->trueColor('primary')
-                ->falseColor('danger'),
+                IconColumn::make('is_active')
+                    ->label('Status')
+                    ->boolean()
+                    ->trueIcon('heroicon-o-check-circle')
+                    ->falseIcon('heroicon-o-x-circle')
+                    ->trueColor('primary')
+                    ->falseColor('danger'),
 
-            Tables\Columns\TextColumn::make('created_at')
-                ->label('Created At')
-                ->dateTime()
-                ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label('Created At')
+                    ->dateTime()
+                    ->sortable(),
 
-            Tables\Columns\TextColumn::make('updated_at')
-                ->dateTime()
-                ->sortable()
-                ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
                 //
--- app/Filament/Resources/WelcomeNoteResource.php
+++ app/Filament/Resources/WelcomeNoteResource.php
@@ -2,18 +2,14 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\WelcomeNoteResource\Pages;
-use App\Filament\Resources\WelcomeNoteResource\RelationManagers;
-use App\Models\WelcomeNote;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
 use Filament\Tables;
-use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Filament\Forms\Set;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use App\Models\WelcomeNote;
 use Illuminate\Support\Str;
+use Filament\Resources\Resource;
 use Filament\Forms\Components\Grid;
 use Filament\Forms\Components\Tabs;
 use Illuminate\Support\Facades\Auth;
@@ -27,8 +23,13 @@ use Illuminate\Database\Eloquent\Model;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\FileUpload;
+use Illuminate\Database\Eloquent\Builder;
 use Filament\Forms\Components\Placeholder;
 use Illuminate\Contracts\Support\Htmlable;
+use Filament\Forms\Components\MarkdownEditor;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\WelcomeNoteResource\Pages;
+use App\Filament\Resources\WelcomeNoteResource\RelationManagers;
 
 class WelcomeNoteResource extends Resource
 {
@@ -43,7 +44,7 @@ class WelcomeNoteResource extends Resource
     protected static ?int $navigationSort = 2;
 
     // name to be used in page titles
-    protected static ?string $modelLabel = 'WelcomeNote';
+    protected static ?string $modelLabel = 'Welcome Note';
 
     // navigation group to be used in navigation
     protected static ?string $navigationGroup = 'Home Contents';
@@ -51,85 +52,97 @@ class WelcomeNoteResource extends Resource
     // slug to be used in route names abd urls
     protected static ?string $slug = 'welcome_note';
 
-     // multiple fields global search with annotation
-   public static function getGloballySearchableAttributes(): array
-   {
-       return ['en_status', 'sw_status'];
-   }
+    // multiple fields global search with annotation
+    public static function getGloballySearchableAttributes(): array
+    {
+        return ['en_status', 'sw_status'];
+    }
+
+    // global search result
+    public static function getGlobalSearchResultTitle(Model $record): string | Htmlable
+    {
+        return $record->sw_status;
+    }
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
                 Section::make()
-                ->description("Fill all required fields on both tabs")
-                ->schema([
-                    Tabs::make('Tabs')
-                        ->tabs([
-                            Tabs\Tab::make('Swahili')
-                                ->schema([
-                                    TextInput::make('sw_status')
-                                        ->label('Cheo')
-                                        ->required(),
+                    ->description("Fill all required fields on both tabs")
+                    ->schema([
+                        Tabs::make('Tabs')
+                            ->tabs([
+                                Tabs\Tab::make('Swahili')
+                                    ->schema([
+                                        TextInput::make('sw_status')
+                                            ->label('Cheo')
+                                            ->required(),
 
-                                    Textarea::make('sw_description')
-                                        ->label('Description')
-                                        ->required(),
+                                        MarkdownEditor::make('sw_description')
+                                            ->disableToolbarButtons([
+                                                'attachFiles',
+                                            ])
+                                            ->label('Maelezo')
+                                            ->required(),
 
-                                ]),
+                                    ]),
 
-                            Tabs\Tab::make('English')
-                                ->schema([
-                                    TextInput::make('en_status')
-                                        ->label('Status')
-                                        ->required(),
+                                Tabs\Tab::make('English')
+                                    ->schema([
+                                        TextInput::make('en_status')
+                                            ->label('Status')
+                                            ->required(),
 
-                                    TextInput::make('slug')
-                                        ->required()
-                                        ->disabled()
-                                        ->dehydrated()
-                                        // ->hidden()
-                                        ->unique(WelcomeNote::class, 'slug', ignoreRecord: true),
+                                        TextInput::make('slug')
+                                            ->required()
+                                            ->disabled()
+                                            ->dehydrated()
+                                            // ->hidden()
+                                            ->unique(WelcomeNote::class, 'slug', ignoreRecord: true),
 
-                                    Textarea::make('en_description')
-                                        ->label('Description')
-                                        ->required(),
+                                        MarkdownEditor::make('en_description')
+                                            ->disableToolbarButtons([
+                                                'attachFiles',
+                                            ])
+                                            ->label('Description')
+                                            ->required(),
 
-                                ])
+                                    ])
 
-                        ])->activeTab(1)->columnSpanFull()
+                            ])->activeTab(1)->columnSpanFull()
 
-                ]),
+                    ]),
 
                 Section::make()
-                ->schema([
-                    TextInput::make('name')
-                    ->label('Name')
+                    ->schema([
+                        TextInput::make('name')
+                            ->label('Name')
+                            ->required()
+                            ->live(onBlur: true)
+                            ->afterStateUpdated(fn(string $operation, $state, Set $set) => $operation
+                                === 'create' ? $set('slug', Str::slug($state)) : null),
+
+                        FileUpload::make('image')
+                            ->image()
+                            ->directory('welcome_notes')
+                            ->columnSpanFull(),
+                    ]),
+                DatePicker::make('created_at')
+                    ->nullable()
+                    ->columnSpanFull(),
+
+                Hidden::make('created_by')
+                    ->default(fn() => Auth::id()),
+
+                Placeholder::make('created_by_name')
+                    ->label('Created By')
+                    ->content(fn() => Auth::user()->name),
+
+                Toggle::make('is_active')
                     ->required()
-                    ->live(onBlur: true)
-                    ->afterStateUpdated(fn(string $operation, $state, Set $set) => $operation
-                        === 'create' ? $set('slug', Str::slug($state)) : null),
-
-                    FileUpload::make('image')
-                        ->image()
-                        ->directory('welcome_notes')
-                        ->columnSpanFull(),
-                        ]),
-                    DatePicker::make('created_at')
-                        ->nullable()
-                        ->columnSpanFull(),
-
-                    Hidden::make('created_by')
-                        ->default(fn() => Auth::id()),
-
-                    Placeholder::make('created_by_name')
-                        ->label('Created By')
-                        ->content(fn() => Auth::user()->name),
-
-                    Toggle::make('is_active')
-                        ->required()
-                        ->default(true)
-                ])->columns(2);
+                    ->default(true)
+            ])->columns(2);
     }
 
     public static function table(Table $table): Table
@@ -137,44 +150,44 @@ class WelcomeNoteResource extends Resource
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make('name')
-                ->label("Name")
-                ->searchable()
-                ->formatStateUsing(function ($state) {
-                    return Str::words($state, 5, '.....');
-                }),
+                    ->label("Name")
+                    ->searchable()
+                    ->formatStateUsing(function ($state) {
+                        return Str::words($state, 5, '.....');
+                    }),
 
                 Tables\Columns\TextColumn::make('image')
-                ->searchable()
-                ->html()
-                ->formatStateUsing(function ($state) {
-                    return '<img src="' . asset('storage/welcome_notes/' . basename($state)) . '" width="30", height="40" />';
-                }),
+                    ->searchable()
+                    ->html()
+                    ->formatStateUsing(function ($state) {
+                        return '<img src="' . asset('storage/welcome_notes/' . basename($state)) . '" width="30", height="40" />';
+                    }),
 
-            Tables\Columns\TextColumn::make('user.name')
-                ->label('Created By')
-                ->searchable(query: function (Builder $query, string $search): Builder {
-                    return $query->whereHas('user', function (Builder $query) use ($search) {
-                        $query->where('name', 'like', "%{$search}%");
-                    });
-                }),
+                Tables\Columns\TextColumn::make('user.name')
+                    ->label('Created By')
+                    ->searchable(query: function (Builder $query, string $search): Builder {
+                        return $query->whereHas('user', function (Builder $query) use ($search) {
+                            $query->where('name', 'like', "%{$search}%");
+                        });
+                    }),
 
-            IconColumn::make('is_active')
-                ->label('Status')
-                ->boolean()
-                ->trueIcon('heroicon-o-check-circle')
-                ->falseIcon('heroicon-o-x-circle')
-                ->trueColor('primary')
-                ->falseColor('danger'),
+                IconColumn::make('is_active')
+                    ->label('Status')
+                    ->boolean()
+                    ->trueIcon('heroicon-o-check-circle')
+                    ->falseIcon('heroicon-o-x-circle')
+                    ->trueColor('primary')
+                    ->falseColor('danger'),
 
-            Tables\Columns\TextColumn::make('created_at')
-                ->label('Created At')
-                ->dateTime()
-                ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label('Created At')
+                    ->dateTime()
+                    ->sortable(),
 
-            Tables\Columns\TextColumn::make('updated_at')
-                ->dateTime()
-                ->sortable()
-                ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
                 //
